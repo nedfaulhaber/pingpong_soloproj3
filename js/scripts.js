2c2096c6fe0d29a1bc7a562cb345df1ffebd6201
Ned <nedfaulhaber@gmail.com>
2016-04-01 11:07:29 -0700

    set up initial html, bootstrap template, css, and js

---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+// business logic
+
+
+
+
+// user logic
+$(document).ready(function(){
+});
+$("form#ping-pong").submit(function(event) {
+  event.preventDefault();
+var number = parseInt($("numberInput").val());

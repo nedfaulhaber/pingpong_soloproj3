1f7cc7a827c6a29ac43c46cc274fe25e5fd83005
Evin Cole <evincole@edwin-faulhabers-macbook-pro.local>
2016-04-11 01:34:04 -0700

    reconstruct js to use Leroi's suggestions regarding populating an empty array and appending

--- js/scripts.js
+++ js/scripts.js
@@ -1,33 +1,46 @@
+//business logic
 
-// if (number % 3 === 0) {     //failed code 3
-//   return "ping"
-// }
+var ping = "Ping";
+var pong = "Pong";
+var pingPongArray = [];
+
+var pingPong = function(number) {
+	for (index = 1; index <= countTo; index += 1) {
+
+		if ((index % 3 === 0) && (index % 5 === 0))  {
+		    pingPongArray.push(ping + pong);
+		}
+     else if (index % 3 === 0){
+	  	pingPongArray.push(ping);
+		}
+
+    else if (index % 5 === 0){
+				pingPongArray.push(pong);
+		}
+
+    else {
+			pingPongArray.push(index);
+	  }
 
 
+	}
+};
 
-
-
+//user interface logic
 
 $(document).ready(function() {
   $("form#ping-pong").submit(function(event) {
     event.preventDefault();
 
-    var countTo = parseInt($("input#countTo").val());
-    // var number = index;  (part of failed code 3)
-    // var pingList = "";  (part of failed code 2)
-    for (var index = 1; index <= countTo; index +=1) {
-      // if (index % 3 === 0) {   //failed code 1
-      //   return "ping"
-      // }
+    var countTo = parseInt($("#countTo").val());
 
-      // if (index % 3 === 0) {   //failed code 2
-      //   pingList += "ping";
-      // }
+    pingPong(countTo);
 
-      $("#result").append(index + ", ")
-    };
+    pingPongArray.forEach(function(number) {
+      $("#result").append("<li>" + number + "</li>");
+    });
 
-    $("#result").show();
+
 
   });
 });
